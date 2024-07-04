55c132556ea0b2e116b1f63de82a3262eb4ee096
tuanle13-git <tuanlv1303@gmail.com>
2024-07-04 23:51:55 +0700

    add layout footer

---
+++ layout/footer.php
@@ -0,0 +1,6 @@
+<footer class="container-fluid text-center p-5" style="background-color: rgb(250,247,240); border-top: 1px solid black;">
+    <h4>ĐÔ ÁN CÁ NHÂN CỦA LÊ VĂN TUẤN</H6>
+    <H3>FACEBOOK: </H3>
+    <H3>GMAIL: </H3>
+    <H3>GITHUB: </H3>
+</footer>
